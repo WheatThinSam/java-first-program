7a4e9ae6226b1967ed27ad84d4c76187a6798dbd
Ruben Samuel Gonzalez <55163305+WheatThinSam@users.noreply.github.com>
2022-01-04 22:45:46 -0600

    Completed module

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -53,7 +53,7 @@ public class Finance {
 
     public static void main(String[] args) {
         String command = args[0];
-        if (!commandsToUsage.containsKey(command)) {
+        if (commandsToUsage.containsKey(command)) {
             System.out.println(command + ": command not found");
             return;
         }
